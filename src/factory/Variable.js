dfba401aad88fc5f3cb48585611d96e4f13ae6ff
mesecret <245454795@qq.com>
2018-02-02 20:24:06 +0800

    haha

--- src/factory/Variable.js
+++ src/factory/Variable.js
@@ -4,9 +4,11 @@ export default{
         // Vue.prototype.variable = {};
         // Vue.prototype.variable.name = 'haha';
         // Vue.prototype.variable.userMoney = 10000000;
+        // Vue.prototype.farmData.userInfo.money = 10000000;
         // if(window.localStorage && window.localStorage.farmData){
         //     var farmData = JSON.parse(window.localStorage.farmData);
         //     Vue.prototype.variable.userMoney = farmData.userInfo.money;
+        //     Vue.prototype.farmData.userInfo.money = farmData.userInfo.money;
         //     Vue.prototype.variable.farmData = farmData;
         // }
     }
